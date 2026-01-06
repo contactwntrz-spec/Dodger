10140554740bc10820cd6cfbc4403589929a6769
Tobias <to.russ355@icloud.com>
2026-01-06 12:08:51 +0100

    Initial Commit

---
+++ Dodger/DodgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodgerApp.swift
+//  Dodger
+//
+//  Created by Tobias Ruß on 06.01.26.
+//
+
+import SwiftUI
+
+@main
+struct DodgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
